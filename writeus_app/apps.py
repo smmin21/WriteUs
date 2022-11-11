2efc66099085560f7e1ef443e6a792af2dd90e28
smmin21 <nstern999@gmail.com>
2022-11-11 19:49:14 +0900

    add koGPT2

--- writeus_app/apps.py
+++ writeus_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from .gpt.kogpt2 import kogpt2
 
 
 class WriteusAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "writeus_app"
+    generator = kogpt2()
--- writeus_app/views.py
+++ writeus_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from .models import *
+from .apps import WriteusAppConfig
 
 # Create your views here.
 def home(request):
@@ -17,8 +18,13 @@ def create(request, nID):
     if (request.method == 'POST'):
         novel.body += ' ' + novel.tmp_body
         novel.tmp_body = request.POST['body']
+        novel.tmp_body += " " + WriteusAppConfig.generator.text_generator(novel.tmp_body)
         novel.save()
         return redirect('create', novel.id)
+    else:
+        if novel.body == '':
+            novel.tmp_body += " " + WriteusAppConfig.generator.text_generator(novel.tmp_body)
+            novel.save()
     ctx = {
         'novel':novel,
     }
